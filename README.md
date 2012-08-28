e3306f5643529ca4dafc12d89f6a43746682cca2
Lin Salisbury <lsalisbury@netflix.com>
2012-08-28 00:44:17 -0700

    Adding a little more to the README.  Changing the package name to something that's not already taken:/

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 Tornado Redis Client
 ====================
 
-Supports Redis <=2.4.16 (2.6.0 support coming)
+An Async redis client that utilizes the Tornado IOLoop.
+
+Supports all commands available with redis 2.4.16.
+
+Installing
+----------
+
+From git repo:
+
+    sudo python setup.py install
+
+Via pip:
+    pip install redis-tornado
+
+
+Usage
+-----
+
+Tests
+-----
+
+Running tests requires that a redis server be available on :6379 (default port)
+
+Execute the runtests shell script (requires python-nose):
+    
+    ./runtests.sh
+
+Or if you are so inclined, run the tests directly:
+
+    PYTHONPATH=. && python ./tests/tests.py
 
--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-#!/usr/bin/python2
+from setuptools import setup, find_packages
 
-from setuptools import setup
+setup(
+    name = 'redis-tornado',
+    version = '0.2',
+    description = 'Async redis client built on the Tornado IOLoop.',
 
-setup(name='tornado-redis',
-    version='0.2',
-    description='Redis client built on the Tornado IOLoop',
-    author='Lin Salisbury',
-    author_email='lin.salisbury@gmail.com',
-    url='http://www.tweezercode.com',
-    packages=['redis'],
-    install_requires=['tornado'])
+    author = 'Lin Salisbury',
+    author_email = 'lin.salisbury@gmail.com',
+
+    url = 'http://www.tweezercode.com',
+
+    license = 'APACHE',
+
+    packages = find_packages(),
+
+    install_requires = [
+        'tornado >= 2.2.1'
+    ],
+)
